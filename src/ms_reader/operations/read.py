3c030320b3c1d5bf8c6f8a8ed4100cf3c042eeb7
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-30 23:33:07 +0000

    phase centre reading added

--- src/ms_reader/operations/read.py
+++ src/ms_reader/operations/read.py
@@ -16,3 +16,23 @@ class Read:
 
     ms_dir: str
     """"""
+
+    @property
+    def phase_centre(self) -> SkyCoord:
+        """
+        """
+        try:
+            with tools.block_logging():
+                phase_centre = table(
+                    os.path.join(self.ms_dir, "FIELD")
+                ).getcol("PHASE_DIR")
+        except:
+            raise FileNotFoundError(
+                "expected a 'FIELD' table with a 'PHASE_DIR' column"
+            )
+        if np.array(phase_centre).shape != (1, 1, 2):
+            raise ValueError("unsupported phase centre definition")
+        
+        return SkyCoord(
+            phase_centre[0][0][0], phase_centre[0][0][1], unit="rad"
+        )

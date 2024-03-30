c38c80093e58d47025ded8507e8660c04c743879
Ali Taqi <alitaqi94.developer@gmail.com>
2024-03-30 23:33:56 +0000

    uvw reading added

--- src/ms_reader/operations/read.py
+++ src/ms_reader/operations/read.py
@@ -36,3 +36,19 @@ class Read:
         return SkyCoord(
             phase_centre[0][0][0], phase_centre[0][0][1], unit="rad"
         )
+
+    @property
+    def uvw(self) -> NDArray:
+        """
+        """
+        try:
+            with tools.block_logging():
+                uvw = table(self.ms_dir).getcol("UVW")
+        except:
+            raise FileNotFoundError("expected a 'UVW' column")
+        if len(np.asarray(uvw).shape) > 3:
+            raise ValueError("unsupported UVW with more than 3 dimensions")
+        if np.asarray(uvw).shape[-1] != 3:
+            raise ValueError("there must be 3 positional coordinates per observation")
+        
+        return np.asarray(uvw)
